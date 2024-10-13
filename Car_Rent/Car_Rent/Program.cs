b0103ed007c5b2299b530c1ba73e355f71815715
Nobodu-1239 <abolfazl.shkofthe.1239+2@gmail.com>
2024-10-13 14:40:36 +0330

    Managing-Car

---
+++ Car_Rent/Car_Rent/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car_RentEntities database = new Car_RentEntities();
+
+            Console.WriteLine("/rent => rent car");
+            Console.WriteLine("/remove => remove");
+            Console.WriteLine("/show => show customer and car's");
+            Console.WriteLine("/exit= > exit console");
+            Console.WriteLine("__________________________________");
+            string resualt = Console.ReadLine();
+
+            switch (resualt)
+            {
+                case "/rent":
+                    Console.WriteLine("enter name - lastname - national number - place");
+                    customer customer = new customer
+                    {
+                        name = Console.ReadLine(),
+                        lastname = Console.ReadLine(),
+                        nationalnumber = Console.ReadLine(),
+                        place = Console.ReadLine(),
+                    };
+                    database.customers.Add(customer);
+                    database.SaveChanges();
+                    Console.WriteLine("enter car name and price");
+                    car car = new car
+                    {
+                        carname = Console.ReadLine(),
+                        price = Console.ReadLine(),
+                    };
+                    database.cars.Add(car);
+                    database.SaveChanges();
+                    Console.WriteLine("finished");
+                    break;
+
+                case "/show":
+                    var item = (from a in database.customers
+                                join b in database.cars on a.Id equals b.Id
+                                select new
+                                {
+                                    a.Id,
+                                    a.name,
+                                    a.lastname,
+                                    a.nationalnumber,
+                                    a.place,
+                                    b.carname,
+                                    b.price,
+                                });
+                    foreach (var list in item)
+                    {
+                        Console.WriteLine($"id : {list.Id}, name : {list.name}, lastname : {list.lastname}, national number : {list.nationalnumber}, palce : {list.place}, car : {list.carname}, price : {list.price}$");
+                    }
+                    break;
+
+                case "/remove":
+                    Console.WriteLine("enter id");
+                    int ID = Convert.ToInt32(Console.ReadLine());
+                    var resualt1 = database.customers.SingleOrDefault(n => n.Id == ID);
+                    database.customers.Remove(resualt1);
+                    var resualt2 = database.cars.SingleOrDefault(n => n.Id == ID);
+                    database.cars.Remove(resualt2);
+                    Console.WriteLine("removed successfully");
+                    break;
+
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Managing-Car-
+# Managing-Car
+
+## Introduction
+This project is a simple console application for managing car rentals. It allows users to add customers and cars to a database, display the information, and remove entries.
+
+## Features
+- **Rent a Car**: Add a new customer and car to the database.
+- **Show Information**: Display the list of customers and their rented cars.
+- **Remove Entries**: Delete a customer and their rented car from the database.
+
+### Prerequisites
+- .NET Framework
+- Entity Framework
+
+
